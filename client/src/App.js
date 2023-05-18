67cef8749386d5d1a278dc2d22c4b4a4bf6d3755
YJin33 <89703649+YJin33@users.noreply.github.com>
2023-05-18 16:41:23 +0900

    파일 초기 설정
    npm install 및 react 세팅

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Routes>
+        <Route exact path="/" element={<LandingPage />} />
+      </Routes> */}
+      APP화면이 보일 자리
+      <br />
+      여기에 라우팅해줄 예정
+    </div>
+  );
+}
+
+export default App;
