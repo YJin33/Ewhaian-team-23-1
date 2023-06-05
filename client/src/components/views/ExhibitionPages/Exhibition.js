127ce66cebe4591f0727a838e3240c9179d3d1ad
rkdmsslasla <gaeun9477@gmail.com>
2023-06-05 19:05:10 +0900

    exhibition.js 프론트 1차

---
+++ client/src/components/views/ExhibitionPages/Exhibition.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+function Exhibition() {
+
+    return (
+        <div className="exhibition">
+            <div className="exhibition_inner_container">
+                <div className="exhibition_top_container">
+                    <div className="exhibition_title_container">
+                        Top3
+                    </div>
+                    <div className="exhibition_top_button_container">
+                        추천순/최신순
+                    </div>
+                </div>
+                <div className="exhibition_body_container">
+                    <div className="exhibition_body_painting_container">
+                        <div className="exhibition_painting_first_line">
+
+                        </div>
+                        <div className="exhibition_painting_second_line">
+
+                        </div>
+                        <div className="exhibition_painting_third_line">
+
+                        </div>
+                    </div>
+                    <div className="exhibition_body_scorll_container">
+                        (스크롤)
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Exhibition;
